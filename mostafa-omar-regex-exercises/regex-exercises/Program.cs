0dd6c4241fc12512ecc494780ba2bace59eed07c
Mostafa Omar <mostafaomar9751@gmail.com>
2025-07-11 16:00:14 +0300

    Add My Folder

---
+++ mostafa-omar-regex-exercises/regex-exercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace regex_exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
+
